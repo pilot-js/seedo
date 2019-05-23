140f47c0183bc9e67f97df0ebf9e18e6adb81ae9
Theo Manton <theo333@gmail.com>
2019-05-23 15:35:17 -0400

    fix: linting issues

--- server/db/models/Challenge.js
+++ server/db/models/Challenge.js
@@ -2,28 +2,28 @@ const conn = require('../conn');
 const { Sequelize } = conn;
 
 module.exports = conn.define('challenge', {
-	// from associations: imageId
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		validate: {
-			notEmpty: true,
-		},
-	},
-	description: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		validate: {
-			notEmpty: true,
-		},
-	},
-	difficulty: {
-		type: Sequelize.INTEGER,
-		allowNull: false,
-		validate: {
-			notEmpty: true,
-			min: 1,
-			max: 5,
-		},
-	},
+  // from associations: imageId
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  difficulty: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 1,
+      max: 5,
+    },
+  },
 });
