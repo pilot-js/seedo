b5a18d3bcb3b0ee8a88db45816813c59cdbb6726
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-23 16:26:20 -0400

    still working on challenge component

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Challenges from './Challenges';
 
 class App extends Component {
   render() {
---
+++ client/Challenges.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Challenges extends Component{
+    
+    render(){
+        // fake some data to render
+        const ChallengesToRender = [
+            {name: 'challenge1'},
+            {}
+        ]; 
+        return(
+            <div>
+                <ul>
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Challenges;
