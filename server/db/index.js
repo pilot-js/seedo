1187b8f5f9fb24e32d20e7f492b0fa17acbaadd0
Theo Manton <theo333@gmail.com>
2019-05-23 15:10:31 -0400

    feat: successfully syncAndSeed data

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,53 @@
 const conn = require('./conn');
+const {
+	usersSeed,
+	userchallengeSeed,
+	challengesSeed,
+	solutionsSeed,
+	imagesSeed,
+} = require('./seed');
+
+const {
+	Challenge,
+	Image,
+	Solution,
+	User,
+	Userchallenge,
+} = require('./models');
 
 const syncAndSeed = () => {
-  return conn.sync({ force: true });
+	return conn.sync({ force: true })
+		.then(() => {
+			return Promise.all([
+				Promise.all(usersSeed.map(user => User.create(user))),
+				Promise.all(challengesSeed.map(chal => Challenge.create(chal))),
+				Promise.all(userchallengeSeed.map(chal => Userchallenge.create(chal))),
+				Promise.all(solutionsSeed.map(sol => Solution.create(sol))),
+				Promise.all(imagesSeed.map(img => Image.create(img)))
+			]);
+		})
+		.then(([users, challenges, userchallenges, solutions, images]) => {
+			// const chal = userchallenges.find(chal => chal.css.includes('circle'))
+			// console.log(chal.get())
+			return Promise.all([
+				userchallenges.find(chal => chal.css.includes('circle')).update({ userId: 2, challengeId: 1 }),
+				userchallenges.find(chal => chal.css.includes('square')).update({ userId: 3, challengeId: 2 }),
+				solutions.find(sol => sol.css.includes('circle')).update({ challengeId: 1 }),
+				solutions.find(sol => sol.css.includes('square')).update({ challengeId: 2 }),
+				images.find(img => img.url.includes('challenge-1')).update({ challengeId: 1 }),
+				images.find(img => img.url.includes('userchallenge-1')).update({ userchallengeId: 1 }),
+				images.find(img => img.url.includes('challenge-2')).update({ challengeId: 2 }),
+				images.find(img => img.url.includes('userchallenge-2')).update({ userchallengeId: 2 }),
+			])
+		})
+		.catch(err => console.log(err));
+};
+
+module.exports = {
+	syncAndSeed,
+	Challenge,
+	Image,
+	Solution,
+	User,
+	Userchallenge,
 };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+const { syncAndSeed } = require('./db');
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+syncAndSeed();
