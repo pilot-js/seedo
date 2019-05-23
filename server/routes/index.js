e53580cdd0a6891d089f77ab9602bf12f07fa55c
Theo Manton <theo333@gmail.com>
2019-05-23 13:24:42 -0400

    note: create routes outline

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+const pluralize = require('pluralize');
+
+// TODO tailor to this project
+// app.use('/auth', require('./routes/user'));
+
+// let routearray = ['product', 'category', 'cart', 
+//                     'lineitem', 'order', 'address', 
+//                     'creditcard', 'review'];
+// routearray.forEach(item => {
+//     app.use(`/api/${pluralize(item)}`, require(`./routes/${item}`));
+// });
