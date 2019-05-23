276be4fe04b60b485eb7c8eae4eae3b7bc6a8f95
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-23 16:32:50 -0400

    update testing due to PR21

--- test/db/userChallenge.test.js
+++ test/db/userChallenge.test.js
@@ -1,22 +1,22 @@
 require('@babel/polyfill');
-const { UserChallenge } = require('../../server/db/models');
+const { Userchallenge } = require('../../server/db/models');
 const db = require('../../server/db/conn');
 
-describe('testing UserChallenge model', () => {
+describe('testing Userchallenge model', () => {
     beforeAll(async () => {
         await db.sync({force: true});
-        await UserChallenge.create({html: 'TEXT', css: 'TEXT', js: 'TEXT', grade: 90, submited: true, image: 'STRING'});
+        await Userchallenge.create({html: 'TEXT', css: 'TEXT', js: 'TEXT', grade: 90, submited: true, imageId: 1});
     });
     afterAll(async () => {
         await db.close();
     });
     it('can retrieve data', async () => {
-        const userChallenges = await UserChallenge.findAll();
-        return expect(userChallenges.length).toBe(1);
+        const userchallenges = await Userchallenge.findAll();
+        return expect(userchallenges.length).toBe(1);
     });
     it('can create a userChallenge', async () => {
-        await UserChallenge.create({html: 'TEXT1', css: 'TEXT1', js: 'TEXT1', grade: 0, submited: false, image: 'STRING'});
-        const allChallenge = await UserChallenge.findAll();
-        return expect(allChallenge.length).toBe(2); 
+        await Userchallenge.create({html: 'TEXT1', css: 'TEXT1', js: 'TEXT1', grade: 0, submited: false, imageId: 2});
+        const allchallenge = await Userchallenge.findAll();
+        return expect(allchallenge.length).toBe(2); 
     });
 })
