e65257c437f5bd18228044fc3198459b5c94703c
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-23 16:42:16 -0400

    fix keys according to lint

--- client/Challenges.js
+++ client/Challenges.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
 
-class Challenges extends Component{
-    
-    render(){
-        // fake some data to render
-        const ChallengesToRender = [
-            {name: 'challenge1', description: 'draw a circle', difficulty: 1},
-            {name: 'challenge2', description: 'draw a square', difficulty: 1}
-        ]; 
-        return(
-            <div>
-                <ul>
-                    {
-                       ChallengesToRender.map((challenge, idx)=>{
-                           return (
-                               <li key={idx}>
-                                    {`${challenge.name}: ${challenge.description}`}
-                               </li>
-                           )
-                       }) 
-                    }
-                </ul>
-            </div>
-        )
-    }
+class Challenges extends Component {
+  render() {
+    // fake some data to render
+    const ChallengesToRender = [
+      { name: 'challenge1', description: 'draw a circle', difficulty: 1 },
+      { name: 'challenge2', description: 'draw a square', difficulty: 1 },
+    ];
+    return (
+      <div>
+        <ul>
+          {ChallengesToRender.map((challenge, idx) => {
+            return <li key={`${challenge.name}${idx}`}>{`${challenge.name}: ${challenge.description}`}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Challenges;
