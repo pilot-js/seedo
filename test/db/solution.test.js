90b9bcfce057516fa42399f3d10753d8bdcf9f42
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-23 20:59:05 -0400

    fix typos

--- test/db/solution.test.js
+++ test/db/solution.test.js
@@ -5,7 +5,7 @@ const db = require('../../server/db/conn');
 describe('testing solution model', () => {
     beforeAll(async () => {
         await db.sync({force: true});
-        await Solution.create({html: 'html TEXT', css: 'css TEXT', js: 'js TEXT', ChallengeId: 1});
+        await Solution.create({html: 'html TEXT', css: 'css TEXT', js: 'js TEXT', challengeId: 1});
     })
     afterAll(async () => {
         await db.close();
@@ -15,7 +15,7 @@ describe('testing solution model', () => {
         return expect(solutions.length).toBe(1);
     })
     it('can create a solution for a challenge', async () => {
-        await Solution.create({html: 'html TEXT1', css: 'css TEXT', js: 'js TEXT', ChallengeId: 2});
+        await Solution.create({html: 'html TEXT1', css: 'css TEXT', js: 'js TEXT', challengeId: 2});
         const allSolutions = await Solution.findAll();
         return expect(allSolutions.length).toBe(2); 
     })
--- test/db/userChallenge.test.js
+++ test/db/userChallenge.test.js
@@ -5,7 +5,7 @@ const db = require('../../server/db/conn');
 describe('testing Userchallenge model', () => {
     beforeAll(async () => {
         await db.sync({force: true});
-        await Userchallenge.create({html: 'TEXT', css: 'TEXT', js: 'TEXT', grade: 90, submited: true, imageId: 1});
+        await Userchallenge.create({html: 'TEXT', css: 'TEXT', js: 'TEXT', grade: 90, submitted: true, imageId: 1});
     });
     afterAll(async () => {
         await db.close();
@@ -15,7 +15,7 @@ describe('testing Userchallenge model', () => {
         return expect(userchallenges.length).toBe(1);
     });
     it('can create a userChallenge', async () => {
-        await Userchallenge.create({html: 'TEXT1', css: 'TEXT1', js: 'TEXT1', grade: 0, submited: false, imageId: 2});
+        await Userchallenge.create({html: 'TEXT1', css: 'TEXT1', js: 'TEXT1', grade: 0, submitted: false, imageId: 2});
         const allchallenge = await Userchallenge.findAll();
         return expect(allchallenge.length).toBe(2); 
     });
