37429a9ae26491f5cfe1cf475ae552d6db886579
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-27 15:03:39 -0400

    adding in code mirror to individual challenge

--- client/components/IndividualChallenge.js
+++ client/components/IndividualChallenge.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
+import CodeMirror from 'react-codemirror';
 
-export const IndividualChallenge = () => {
-  const challenge = { name: 'challenge1', description: 'draw a circle', difficulty: 1 };
-  return (
-    <div className="d-flex flex-column align-items-center">
-      <h1>{challenge.name}</h1>
-      <p>{challenge.description}</p>
-      <div>
-        <div className="row">
-          <div className="col">users page goes here</div>
-          <div className="col">our image goes here</div>
-        </div>
-        <div className="row">
-          <div className="col">HTML Editor</div>
-          <div className="col">CSS Editor</div>
-          <div className="col">JS Editor</div>
+export class IndividualChallenge extends Component{
+  constructor(){
+    super()
+    this.state = {
+      name: 'CodeMirror',
+      code: '',
+      codeHTML: '',
+      codeCSS: '',
+      codeJS: ''
+    }
+    this.updateCode = this.updateCode.bind(this)
+    this.changeValue = this.changeValue.bind(this)
+  }
+
+  updateCode(newCode){
+    this.setState({
+      code: newCode
+    })
+  }
+
+  changeValue(ev){
+    // deal with the code:
+    this.setState({[ev.target.name]: this.state.code}, ()=>console.log(this.state))
+  }
+
+  render(){
+    const challenge = { name: 'challenge1', description: 'draw a circle', difficulty: 1 };
+    let options = {
+      lineNumbers: true,
+      mode: 'javascript'
+    }
+    return (
+      <div className="d-flex flex-column align-items-center">
+        <h1>{challenge.name}</h1>
+        <p>{challenge.description}</p>
+        <div>
+          <div className="row">
+            <div className="col">users page goes here</div>
+            <div className="col">our image goes here</div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h4>HTML Editor</h4>
+              <CodeMirror name='codeHTML' value={this.state.code} onChange={this.updateCode} options={options}/>
+              <button name="codeHTML" type="button" onClick={this.changeValue}>run</button>
+            </div>
+            <div className="col">
+              <h4>CSS Editor</h4>
+              <CodeMirror name='codeCSS' value={this.state.code} onChange={this.updateCode} options={options}/>
+              <button name="codeCSS" type="button" onClick={this.changeValue}>run</button>
+            </div>
+            <div className="col">
+              <h4>JS Editor</h4>
+              <CodeMirror name='codeJS' value={this.state.code} onChange={this.updateCode} options={options}/>
+              <button name="codeJS" type="button" onClick={this.changeValue}>run</button>
+            </div>   
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
