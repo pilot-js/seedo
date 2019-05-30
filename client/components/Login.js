add9124bb91cb7535c78b119806db575b5d0fb6b
Kristy Zhang <kristyso17@gmail.com>
2019-05-30 12:51:42 -0400

    test1

--- client/components/Login.js
+++ client/components/Login.js
@@ -5,6 +5,8 @@ import { getUser } from '../store';
 const LoginClass = props => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [CLIENT_ID, setClientId] = useState('712a1372abccbaf43bfc');
+  const [CLIENT_SECRET, setSecret] = useState('1b9a5bbf6e46b74a52faf3c108f7acee671c82ae');
 
   const saveHandler = ev => {
     ev.preventDefault();
@@ -12,6 +14,10 @@ const LoginClass = props => {
       .getUser({ email, password })
       .then(() => console.log('We have a user logged in.'))
       .then(() => props.history.push('/'));
+  };
+
+  const GithubOauth = () => {
+    window.location.href=`https://github.com/login/oauth/authorize?client_id${CLIENT_ID}`
   };
 
   return (
@@ -33,6 +39,7 @@ const LoginClass = props => {
         />
         <button type="submit">Login</button>
       </form>
+      <button onClick={GithubOauth}> Login With Github </button>
     </div>
   );
 };
