bea5ca898136bf0a21e6e4f0c47aebc40af436c6
Kristy Zhang <kristyso17@gmail.com>
2019-05-30 10:24:11 -0400

    oauth setup

--- server/app.js
+++ server/app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
+const ejs = require('ejs');
 const api = require('./routes/api');
+
+try {
+  Object.assign(process.env, require('./.env'));
+} catch (error) {
+  console.log(error);
+}
+
+app.engine('html', ejs.renderFile);
 
 app.use(express.json());
 
@@ -11,5 +20,10 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use(morgan('dev'));
 
 app.use('/api', api);
+
+app.get('/login', (req, res, next) => {
+  const URL=`https://github.com/login/oauth/authorize?client_id${process.env.CLIENT_ID}`
+  res.redirect(URL)
+})
 
 module.exports = app;
