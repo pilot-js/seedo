711e09c45f2fbaca97cf255c9e673680fde12888
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-06-01 10:16:23 -0400

    adding puppeteer to run html and css

---
+++ server/tmp/all_promise.js
@@ -0,0 +1,12 @@
+function All(promises){
+    return new Promise((res, rej)=>{
+        const results = []
+        let counter = 0
+        promises.forEach((promise, idx)=>{
+            promise.then(result => {
+                results[idx] = result;
+                counter += 1;
+            })
+        })
+    })
+}
