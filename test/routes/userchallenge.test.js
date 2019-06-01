82b26e753229b1821ba2687ecca3ebb8e856665f
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-06-01 18:29:57 -0400

    update test routes

--- test/routes/userchallenge.test.js
+++ test/routes/userchallenge.test.js
@@ -7,23 +7,20 @@ const { Userchallenge, conn, User, Challenge } = require('../../server/db');
 const client = supertest(app);
 
 const url = '/api/userchallenges/';
-const uuid = '0d5e2df8-3ebd-4139-b752-da1dc4157b77'
 describe('Userchallenge routes', () => {
   beforeAll(async () => {
     await conn.sync({ force: true });
+    const user = await User.create({
+      email: 'email@email.com',
+      password: 'hello',
+    });
     await Userchallenge.create({
       html: 'TEXT',
       css: 'TEXT',
       js: 'TEXT',
       grade: 90,
       submitted: true,
-      userId: uuid,
-    });
-    const ucs = await Userchallenge.findAll();
-    console.log('Our user challenges', ucs);
-    await User.create({
-      email: 'email@email.com',
-      password: 'hello',
+      userId: user.id,
     });
     await Challenge.create({
       name: 'Basic html page',
@@ -34,10 +31,18 @@ describe('Userchallenge routes', () => {
   afterAll(async () => {
     await conn.close();
   });
-  it('it can take a request', () => {
+  it('it can take a request', async () => {
+    const user = await User.findOne({ where: { email: 'email@email.com'}})
     return client
       .put(`${url}1`)
-      .send({ userAnswer: {} })
+      .send({ userAnswer: { 
+        userId: user.id,
+        html: 'TEXT',
+        css: 'TEXT',
+        js: 'TEXT',
+        grade: 90,
+        submitted: true,  
+      } })
       .expect(200);
   });
 });
