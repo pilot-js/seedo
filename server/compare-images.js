3a280f8f96873b5a046e924bd549ad2b1b117bc9
Theo Manton <theo333@gmail.com>
2019-06-02 17:06:02 -0400

    fix: linting issues

--- server/compare-images.js
+++ server/compare-images.js
@@ -3,7 +3,6 @@ const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
 
 const compareImages = (userchallengePath, challengeImg) => {
-
   // const img1 = fs
   //   .createReadStream(challengeImg)
   //   .pipe(new PNG())
@@ -13,22 +12,16 @@ const compareImages = (userchallengePath, challengeImg) => {
   //   .pipe(new PNG())
   //   .on('parsed', doneReading);
   // let filesRead = 0;
-
   // function doneReading() {
   //   if (++filesRead < 2) return;
   //   const diff = new PNG({ width: img1.width, height: img1.height });
-
   //   const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {
   //     threshold: 0.1,
   //   });
-
   //   diff.pack().pipe(fs.createWriteStream('diff2.png'));
-
   //   const percentMatch = Math.round(100 * (1 - numDiffPixels / (img1.width * img1.height)));
-
   //   console.log('percentMatch: ', percentMatch);
   // }
-
 };
 
 module.exports = {
