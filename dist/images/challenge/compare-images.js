6bf0ff21c1826b1b7166780896b6c5b9dc3c4d44
Theo Manton <theo333@gmail.com>
2019-06-03 19:07:12 -0400

    WIP compare images

--- dist/images/challenge/compare-images.js
+++ dist/images/challenge/compare-images.js
@@ -2,12 +2,15 @@ var fs = require('fs'),
   PNG = require('pngjs').PNG,
   pixelmatch = require('pixelmatch');
 
-var img1 = fs.createReadStream('challenge-1-red-circle.png').pipe(new PNG()).on('parsed', doneReading),
-  img2 = fs.createReadStream('challenge-1-red-circle2.png').pipe(new PNG()).on('parsed', doneReading),
-  filesRead = 0;
+const img1 = fs.createReadStream('challenge-1-red-circle.png')
+  .pipe(new PNG())
+  .on('parsed', doneReading);
+const img2 = fs.createReadStream('challenge-1-red-circle2.png').pipe(new PNG()).on('parsed', doneReading);
+let filesRead = 0;
 
 function doneReading() {
   if (++filesRead < 2) return;
+  console.log('img1:', img1.width, img1.height)
   var diff = new PNG({ width: img1.width, height: img1.height });
 
   const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, { threshold: 0.1 });
