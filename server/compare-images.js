d8afb6ee0a8ebd1a1fc1662e078c2045f928bfd9
Theo Manton <theo333@gmail.com>
2019-06-04 19:47:56 -0400

    fix: linting issues

--- server/compare-images.js
+++ server/compare-images.js
@@ -1,6 +1,7 @@
 /* eslint no-use-before-define: 1 */
 const fs = require('fs');
-const btoa = require('btoa');
+// const btoa = require('btoa');
+const stream = require('stream');
 const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
 
@@ -12,23 +13,29 @@ function compareImages(userchallengePath, challengeImg, userId) {
   // const img1 = challengeImg;
   // let buff = Buffer.from(challengeImg);
   // fs.writeFileSync('chall.png', challengeImg);
-  const newUint8Array = btoa(String.fromCharCode(...new Uint8Array(challengeImg.data)));
-  console.log('newUint8Array: ', newUint8Array);
+  // const newUint8Array = btoa(String.fromCharCode(...new Uint8Array(challengeImg.data)));
+  // console.log('newUint8Array: ', newUint8Array);
   // const base64String = btoa(String.fromCharCode(...new Uint8Array(challengeImg.data)));
   // console.log('Base64 image data converted to file: ', base64String)
 
-  const img1 = fs
-    .createReadStream(newUint8Array)
-    .pipe(new PNG())
-    .on('parsed', doneReading);
+  const buffStream = new stream.PassThrough();
+  buffStream.end(challengeImg.data);
+  const img1 = buffStream.pipe(new PNG()).on('parsed', doneReading);
+  // const img1 = fs
+  //   .createReadStream(challengeImg.data)
+  //   .pipe(new PNG())
+  //   .on('parsed', doneReading);
   const img2 = fs
     .createReadStream(userchallengePath)
     .pipe(new PNG())
     .on('parsed', doneReading);
-  const filesRead = 0;
+  let filesRead = 0;
 
   function doneReading() {
-    // if (++filesRead < 2) return;
+    if (++filesRead < 2) {
+      console.log('First file completed reading.');
+      return;
+    }
     console.log('img1.data: ', img1.data);
     const diff = new PNG({ width: img1.width, height: img1.height });
     const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {
---
+++ server/test-compareImages.js
@@ -0,0 +1,5 @@
+const compareImages = require('./compare-images');
+
+const path =
+  '/Users/Theo/Documents/fullstack/projects/capstone/server/tmp/25328a3f-1687-492a-bfd5-78223baadc50.png';
+compareImages();
