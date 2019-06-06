0803d417ee4ed792d76b35e9c6644d14debeb580
Theo Manton <theo333@gmail.com>
2019-06-05 21:51:34 -0400

    fix: delete unneeded file and dependency

--- server/test-compareImages.js
+++
@@ -1,5 +0,0 @@
-const compareImages = require('./compare-images');
-
-const path =
-  '/Users/Theo/Documents/fullstack/projects/capstone/server/tmp/25328a3f-1687-492a-bfd5-78223baadc50.png';
-compareImages();
