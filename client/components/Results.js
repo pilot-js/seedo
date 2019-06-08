6bdb3831f6951708f4483b2d1995e7c962fd7825
Grant Horner <h.grant.horner@gmail.com>
2019-06-08 14:03:59 -0400

    set up results modal

---
+++ client/components/Results.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+export const Results = ({ userImage, solutionImage, diffImage, grade, closeModal }) => (
+  <div>
+    <div className="d-flex justify-content-between row">
+      <img src={userImage} alt="Yours" />
+      <img src={solutionImage} alt="Solution" />
+      <img src={diffImage} alt="Diff" />
+    </div>
+    <div className="d-flex justify-content-between row">
+      <h4>Your grade: {grade}</h4>
+      <div className="btn-group" role="group">
+        <button className="btn btn-sm btn-success" type="button">
+          Submit answer!
+        </button>
+        <button className="btn btn-sm btn-info" onClick={closeModal} type="button">
+          Continue designing...
+        </button>
+      </div>
+    </div>
+  </div>
+);
--- client/index.js
+++ client/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Modal from 'react-modal';
 import { store } from './store';
 import App from './App';
 
 const root = document.querySelector('#root');
+
+// used to help screen readers
+Modal.setAppElement(root);
 
 ReactDOM.render(
   <Provider store={store}>
