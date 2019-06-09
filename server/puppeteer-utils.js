df34820f75a7f4951f6e76e67480a0d49371b368
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-06-09 16:38:04 -0400

    fix lint

--- server/puppeteer-utils.js
+++ server/puppeteer-utils.js
@@ -19,31 +19,31 @@ const parseHTML = (html, userId) => {
   </html>`;
 };
 
-const createFiles = async (html, css, userId) => {
-  await fs.mkdir('./server/tmp', { recursive: true }, err => {
+const createFiles = async (html, css, userId, dir) => {
+  await fs.mkdir(dir, { recursive: true }, err => {
     if (err) throw err;
-    console.log('create dir');
+    console.log('create dir: ', dir);
   });
-  await fs.writeFile(`./server/tmp/${userId}.html`, parseHTML(html, userId), err => {
+  await fs.writeFile(`${dir}${userId}.html`, parseHTML(html, userId), err => {
     if (err) throw err;
     console.log('The html has been saved!');
   });
-  await fs.writeFile(`./server/tmp/${userId}.css`, css, err => {
+  await fs.writeFile(`${dir}${userId}.css`, css, err => {
     if (err) throw err;
     console.log('The css has been saved!');
   });
 };
 
-const createImage = async (userId, challengeId) => {
+const createImage = async (userId, challengeId, dir) => {
   try {
     const image = await Image.findOne({ where: { challengeId } });
     const args = ['-–no-sandbox', '-–disable-setuid-sandbox'];
     const browser = await puppeteer.launch({ args });
     const page = await browser.newPage();
-    const retPath = `file://${path.join(process.cwd(), `server/tmp/${userId}.html`)}`;
+    const retPath = `file://${path.join(process.cwd(), `${dir}${userId}.html`)}`;
     await page.goto(retPath);
     await page.setViewport({ width: image.width, height: image.height });
-    await page.screenshot({ path: `./server/tmp/${userId}.png` });
+    await page.screenshot({ path: `${dir}${userId}.png` });
     await browser.close();
     return retPath;
   } catch (err) {
