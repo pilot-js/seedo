b576550ffbd55105dec1b97b4b852093fba5c880
Theo Manton <theo333@gmail.com>
2019-06-12 22:18:04 -0400

    refactor: add imageWidth and imageHeight to image test but still not working

--- test/db/image.test.js
+++ test/db/image.test.js
@@ -33,7 +33,10 @@ describe('testing image model', () => {
     const image = await Image.create({
       userchallengeId: uc.id,
       data: 'somedata',
+      width: 150,
+      height: 150,
     });
+    console.log('image: ', image.get());
     expect(image).toBeTruthy();
   });
   describe('Image.saveImage tests', () => {
@@ -45,13 +48,13 @@ describe('testing image model', () => {
         grade: 90,
         submitted: true,
       });
-      const image = await Image.saveImage(pathToImage, uc.id);
+      const image = await Image.saveImage(pathToImage, uc.id, true, 150, 150);
       expect(image).toBeTruthy();
     });
     it('can update an image record in the database', async () => {
       const uc = await Userchallenge.findOne({ where: { html: 'TEXT' } });
       const testImage = await Image.findOne({ where: { userchallengeId: uc.id } });
-      const image = await Image.saveImage(pathToImage, uc.id);
+      const image = await Image.saveImage(pathToImage, uc.id, true, 150, 150);
       expect(testImage === image).toBe(false);
       expect(image).toBeTruthy();
     });
