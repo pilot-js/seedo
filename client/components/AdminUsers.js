dee660e6a728088a9565458bf9a237f58e9ffe9a
Theo Manton <theo333@gmail.com>
2019-06-15 10:18:21 -0400

    add table headers to admin users

--- client/components/AdminUsers.js
+++ client/components/AdminUsers.js
@@ -1,5 +1,23 @@
 import React, { useEffect } from 'react';
 
 export const AdminUsers = props => {
-  return <div>Admin users here</div>;
+  return (
+    <div>
+      <h1>Users</h1>
+
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Edit | Archive | Delete</th>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Type</th>
+          </tr>
+        </thead>
+        <tbody />
+      </table>
+    </div>
+  );
 };
