771bf8a3a23dff21470b42b7aa016ca408af5369
Theo Manton <theo333@gmail.com>
2019-06-15 12:54:34 -0400

    change user reducer intial state from arr to obj

--- client/store/user.js
+++ client/store/user.js
@@ -4,7 +4,7 @@ export const SET_USER = Symbol('set user');
 
 export const setUser = user => ({ type: SET_USER, user });
 
-export const user = (state = [], action) => {
+export const user = (state = {}, action) => {
   switch (action.type) {
     case SET_USER:
       return action.user;
