36fbd28c6c3afd16050f26aeb092de2f386e1ff8
Theo Manton <theo333@gmail.com>
2019-06-16 14:38:52 -0400

    update AdminUsers

--- client/components/AdminUsers.js
+++ client/components/AdminUsers.js
@@ -1,38 +1,77 @@
+/* eslint indent: 0 */
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchUsers } from '../store';
 
 const _AdminUsers = props => {
   useEffect(() => {
     props.fetchUsers();
-    console.log('props: ', props);
   }, []);
 
-  console.log('users: ', props.users);
+  const { users } = props;
   return (
     <div>
       <h1>Users</h1>
-
+      <Link to="/admin/users/create">
+        <button type="submit">Add User</button>
+      </Link>
       <table className="table table-striped table-hover">
         <thead>
           <tr>
             <th scope="col">Edit | Archive | Delete</th>
-            <th scope="col">ID</th>
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
             <th scope="col">Email</th>
             <th scope="col">Type</th>
           </tr>
         </thead>
-        <tbody />
+        <tbody>
+          {users
+            ? users.map(user => {
+                const { id, firstName, lastName, email, type } = user;
+                return (
+                  <tr key={id}>
+                    <td>
+                      <div className="btn-group" role="group" aria-label="edit actions">
+                        <Link to={`/admin/users/${id}`}>
+                          <button type="button" className="btn btn-secondary item-edit">
+                            Edit
+                          </button>
+                        </Link>
+                        {/* <Link
+                        to="/"
+                      >
+                        <button type="button" className="btn btn-secondary item-archive">
+                          Archive
+                        </button>
+                      </Link>
+                      <button
+                        type="button"
+                        className="btn btn-secondary item-delete"
+                        onClick={() => deleteUser(id)}
+                      >
+                        Del
+                        </button> */}
+                      </div>
+                    </td>
+                    <td>{firstName}</td>
+                    <td>{lastName}</td>
+                    <td>{email}</td>
+                    <td>{type}</td>
+                  </tr>
+                );
+              })
+            : ''}
+        </tbody>
       </table>
     </div>
   );
 };
 
 const mapStateToProps = ({ users }) => {
-  console.log('users: ', users);
+  // console.log('users: ', users);
   return {
     users,
   };
