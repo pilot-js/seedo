9cbf26bf039f37676c6f6e3137155bedc4ac5295
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-06-19 17:20:10 -0400

    Adding three more challenges for seeding

--- server/db/seed/challenge.js
+++ server/db/seed/challenge.js
@@ -18,9 +18,19 @@ const challengesSeed = [
     difficulty: 2,
   },
   {
-    name: 'Make rectangle',
-    description: 'Make an orange rectangle that is 100px wide and 200px high',
-    difficulty: 2,
+    name: 'Make a clock',
+    description: 'Make a clock with hour, minute, and second hands',
+    difficulty: 5,
+  },
+  {
+    name: 'My home page',
+    description: 'Make an home page that is 540px wide and 304px high',
+    difficulty: 3,
+  },
+  {
+    name: 'squares',
+    description: 'Make a row of four squares, each of them is 40px by 40px',
+    difficulty: 3,
   },
 ];
 
--- server/db/seed/solution.js
+++ server/db/seed/solution.js
@@ -53,22 +53,166 @@ const solutionsSeed = [
     submitted: false,
   },
   {
-    html: `<!DOCTYPE html>
-      <html>
-        <head>
-          <title>Rectangle</title>
-        </head>
-        <body>
-          <div id='rectangle'></div>
-        </body>
-      </html>`,
-    css: `#rectangle {
-        width: 100px;
-        height: 200px;
-        background-color: orange;
-      }`,
+    html: `<div class="clock">
+        <div class="wrap">
+        <span class="hour"></span>
+        <span class="minute"></span>
+        <span class="second"></span>
+        <span class="dot"></span>
+        </div>
+      </div>`,
+    css: `body {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      background: #D9D7DD;
+    }
+    
+    .clock {
+      border-radius: 100%;
+      background: #ffffff;
+      font-family: "Montserrat";
+      border: 5px solid white;
+      box-shadow: inset 2px 3px 8px 0 rgba(0, 0, 0, 0.1);
+    }
+    
+    .wrap {
+      overflow: hidden;
+      position: relative;
+      width: 350px;
+      height: 350px;
+      border-radius: 100%;
+    }
+    
+    .minute,
+    .hour {
+      position: absolute;
+      height: 100px;
+      width: 6px;
+      margin: auto;
+      top: -27%;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background: black;
+      transform-origin: bottom center;
+      transform: rotate(0deg);
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.4);
+      z-index: 1;
+    }
+    
+    .minute {
+      position: absolute;
+      height: 130px;
+      width: 4px;
+      top: -38%;
+      left: 0;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.4);
+      transform: rotate(90deg);
+    }
+    
+    .second {
+      position: absolute;
+      height: 90px;
+      width: 2px;
+      margin: auto;
+      top: -26%;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      border-radius: 4px;
+      background: #FF4B3E;
+      transform-origin: bottom center;
+      transform: rotate(180deg);
+      z-index: 1;
+    }
+    
+    .dot {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 12px;
+      height: 12px;
+      border-radius: 100px;
+      background: white;
+      border: 2px solid #1b1b1b;
+      border-radius: 100px;
+      margin: auto;
+      z-index: 1;
+    }`,
     js: '',
     submitted: false,
+  },
+  {
+    html: `
+      <div>
+        <ul class="navbar">
+          <li>Home Page</li>
+          <li>Musings</li>
+          <li>My town</li>
+          <li>Links</li>
+        </ul>
+        <h1>My page</h1>
+      
+        <p>Welcome to my styled page!</p>
+        
+        <p>It lacks images, but at least it has style.
+        And it has links, even if they don't go
+        anywhere;</p>
+        
+        <p>There should be more here, but I don't know
+        what yet.</p>
+  
+        <span>Made 19 Jun 2019 by myself.</span>
+      </div>`,
+    css: `body {
+        padding-left: 11em;
+        font-family: Georgia, "Times New Roman",
+              Times, serif;
+        color: purple;
+        background-color: #d8da3d }
+        ul.navbar {
+        position: absolute;
+        top: 2em;
+        left: 1em;
+        width: 9em }
+        h1 {
+          font-family: Helvetica, Geneva, Arial,
+              SunSans-Regular, sans-serif }`,
+    js: '',
+  },
+  {
+    html: `<table>
+      <tr>
+          <td class="red"></td>
+          <td class="yellow"></td>
+          <td class="orange"></td>
+          <td class="blue"></td>
+      <tr>
+      </table>`,
+    css: `table{
+        border-collapse: collapse;
+        }
+        td{    
+            padding:40px;
+            border:10px solid black;
+        }
+        .red{
+            background-color:#F15E66;
+        }
+        .yellow{
+            background-color:#FFDB64;
+        }
+        .orange{
+            background-color:#F58326;
+        }
+        .blue{
+            background-color:#85B1DE;
+        }`,
+    js: '',
   },
 ];
 
